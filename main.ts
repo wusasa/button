4b7f62df1146ce8976ccab273b249656f562f6b8
wusasa <105687955+wusasa@users.noreply.github.com>
2022-05-17 10:05:23 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Target)
+})
+basic.forever(function () {
+	
+})
